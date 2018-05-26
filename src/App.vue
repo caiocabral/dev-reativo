d181a03b65dc81091701f47e47e0d0e2c6170bbe
Raphael Gomide <raphael.gomide@gmail.com>
2018-05-26 19:52:25 -0300

    em andamento

--- src/App.vue
+++ src/App.vue
@@ -7,44 +7,13 @@
     <br><br>
     <div class='content'>
       <labeled-input title='Salário bruto:' :onInputChange="updateSalario" />
-      <input title='Base INSS:' :value="salario.baseINSS" disabled />
+      <labeled-input title='Base INSS:' :value="salario.baseINSS" disabled />
       <labeled-input title='Desconto INSS:' :value="salario.descontoINSS" disabled />
       <labeled-input title='Base IRPF:' :value="salario.baseIRPF" disabled />
       <labeled-input title='Desconto IRPF:' :value="salario.descontoIRPF" disabled />
       <labeled-input title='Salário líquido:' :value="salario.salarioLiquido" disabled />
-      <p>{{ salario.salarioLiquido }}</p>
     </div>
-    <div style="text-align: left">
-      <!-- <label>
-        Salário bruto: <input autofocus type='number' v-on:input="updateSalario" />
-      </label>
 
-      <br><br>
-      <label>
-        Base INSS: <input disabled type='number' :value="salario.baseINSS" />
-      </label>
-
-      <br><br>
-      <label>
-        Desconto INSS: <input disabled type='number' :value="salario.descontoINSS" />
-      </label>
-
-      <br><br>
-      <label>
-        Base IRPF: <input disabled type='number' :value="salario.baseIRPF" />
-      </label>
-
-      <br><br>
-      <label>
-        Desconto IRPF: <input disabled type='number' :value="salario.descontoIRPF" />
-      </label>
-
-      <br><br>
-      <label>
-        Salário líquido: <input disabled type='number' :value="salario.salarioLiquido" />
-      </label> -->
-
-    </div>
   </div>
 </template>
 
@@ -57,16 +26,17 @@ export default {
   components: {
     'labeled-input': LabeledInput
   },
+
   data() {
     return {
       msg: 'Raphael',
-      salario: new Salario(1000)
+      salario: new Salario(0)
     };
   },
+
   methods: {
-    updateSalario: event => {
+    updateSalario: function(event) {
       this.salario = new Salario(+event.target.value);
-      console.log(this.salario.salarioLiquido);
     }
   }
 };
